cf5b2c619916379def25372dd8ce800bc663fd1a
Avery McAllister <avery@bradandsylver.com>
2023-11-15 09:12:28 -0600

    added table view code

---
+++ rosterApp/Students.swift
@@ -0,0 +1,15 @@
+//
+//  Students.swift
+//  rosterApp
+//
+//  Created by Avery McAllister on 11/15/23.
+//
+
+import Foundation
+
+class Students{
+    
+    //call as Students.people
+    static var people = [""]
+    
+}
--- rosterApp/ViewControllerClub.swift
+++ rosterApp/ViewControllerClub.swift
@@ -5,25 +5,34 @@
 //  Created by MYKAELA WALLEN on 11/14/23.
 //
 
+
+
+
 import UIKit
 
-class ViewControllerClub: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class ViewControllerClub: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
     
 
-    /*
-    // MARK: - Navigation
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //TableOutlet.delegate = self
+        //TableOutlet.dataSource = self
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Students.people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") as! CrazyCell
+        //return cell
+    }
+
+
 
 }
