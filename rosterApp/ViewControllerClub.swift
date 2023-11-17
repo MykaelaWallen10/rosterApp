86c910260452533124852f6e8a296bddb2208be2
Avery McAllister <avery@bradandsylver.com>
2023-11-17 09:08:55 -0600

    added delete and table

--- rosterApp/ViewControllerClub.swift
+++ rosterApp/ViewControllerClub.swift
@@ -16,6 +16,8 @@ class ViewControllerClub: UIViewController, UITableViewDelegate, UITableViewData
    var x = 0
     
     var students = [Students]()
+    
+  
 
     @IBOutlet weak var TableOutlet: UITableView!
     
@@ -30,31 +32,10 @@ class ViewControllerClub: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         TableOutlet.delegate = self
         TableOutlet.dataSource = self
+       
 
         titleLabel.text = ClubName.title
     }
-    
-    
-    @IBAction func addNameAction(_ sender: UIBarButtonItem) {
-      
-        let name = addNameTextField.text!
-        
-        if(addNameTextField.text?.isEmpty ?? true){
-            
-        }
-        else{
-            var stud = Students(people: addNameTextField.text!)
-            TableOutlet.reloadData()
-        }
-  
-       
-        
-        
-      
-        
-    }
-    
-    
     
     
     
@@ -66,10 +47,36 @@ class ViewControllerClub: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell")!
+        cell.textLabel?.text = String(students[indexPath.row].people)
         return cell
+        
+        
     }
     
+
+       
     
+    @IBAction func addNameAction(_ sender: UIBarButtonItem) {
+        let name = addNameTextField.text!
+        
+        if(addNameTextField.text?.isEmpty ?? true){
+            
+       }
+        else{
+    
+            let stud = Students(people: name)
+           students.append(stud)
+           TableOutlet.reloadData()
+        }
+    
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            students.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
     
     //clicking kid
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
